551f39c4abca1f5cefe71b04a4a93fa10d635b24
clarissawi609-source <clarissawi609@gmail.com>
2026-03-04 22:55:37 +0700

    fix category type and image host

--- next.config.ts
+++ next.config.ts
@@ -4,30 +4,12 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "api.escuelajs.co",
-      },
-      {
-        protocol: "https",
-        hostname: "m.media-amazon.com",
-      },
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-      },
-      {
-        protocol: "https",
-        hostname: "i.imgur.com",
-      },
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-      },
-      {
-        protocol: "https",
-        hostname: "images.macrumors.com",
-      },
+      { protocol: "https", hostname: "api.escuelajs.co" },
+      { protocol: "https", hostname: "m.media-amazon.com" },
+      { protocol: "https", hostname: "images.jdmagicbox.com" },
+      { protocol: "https", hostname: "placehold.co" },
+      { protocol: "https", hostname: "i.imgur.com" },
+      { protocol: "https", hostname: "picsum.photos" },
     ],
   },
 };
